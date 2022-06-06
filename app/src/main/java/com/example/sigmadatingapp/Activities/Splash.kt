8a7130e3fdc3be2064c91e7b764d547649f17b7b
rachit <rachitmishra468@gmail.com>
2022-06-06 20:55:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sigmadatingapp/Activities/Splash.kt
@@ -0,0 +1,12 @@
+package com.example.sigmadatingapp.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sigmadatingapp.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
