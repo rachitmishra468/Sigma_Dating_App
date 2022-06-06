462f3fe4d1b27ccba97a4282c29a25a74383e7a9
rachit <rachitmishra468@gmail.com>
2022-06-06 21:52:16 +0530

    First Commit

--- app/src/main/java/com/example/sigmadatingapp/Activities/Splash.kt
+++ app/src/main/java/com/example/sigmadatingapp/Activities/Splash.kt
@@ -1,12 +1,33 @@
 package com.example.sigmadatingapp.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.sigmadatingapp.R
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        check_login_flag()
+    }
+
+    fun check_login_flag() {
+        Handler().postDelayed(
+            {
+               startActivity(Intent(this,Login_SignUpActivity::class.java))
+
+            },
+            500
+        )
+    }
+
+
 }
