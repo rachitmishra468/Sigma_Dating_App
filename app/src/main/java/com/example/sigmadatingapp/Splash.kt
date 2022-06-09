0acba15618d58854c15938419e85078aedeafdf5
ajay singh <ajayschauhan485@gmail.com>
2022-06-09 22:23:44 +0530

    second commit -ajay 9th june

--- app/src/main/java/com/example/sigmadatingapp/Splash.kt
+++ app/src/main/java/com/example/sigmadatingapp/Splash.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import com.example.sigmadatingapp.Activities.Home
 import com.example.sigmadatingapp.Activities.Login_Activity
+import com.example.sigmadatingapp.Activities.OnBoardingActivity
 import com.example.sigmadatingapp.storage.AppConstants
 import com.example.sigmadatingapp.storage.SharedPreferencesStorage
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,10 +37,10 @@ class Splash : AppCompatActivity() {
             {
 
                 if (sharedPreferencesStorage!!.getBoolean(AppConstants.IS_AUTHENTICATED)) {
-                    startActivity(Intent(this, Home::class.java))
+                    startActivity(Intent(this, OnBoardingActivity::class.java))
                     finish()
                 } else {
-                    startActivity(Intent(this, Home::class.java))
+                    startActivity(Intent(this, OnBoardingActivity::class.java))
                     finish()
                 }
 
