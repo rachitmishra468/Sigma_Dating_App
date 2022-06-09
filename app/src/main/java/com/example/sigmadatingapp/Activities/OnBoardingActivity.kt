0f48d9f3b990916367000778445abf1202c8d261
ajay singh <ajayschauhan485@gmail.com>
2022-06-09 21:18:32 +0530

    second commit -ajay 9th june

---
+++ app/src/main/java/com/example/sigmadatingapp/Activities/OnBoardingActivity.kt
@@ -0,0 +1,58 @@
+package com.example.sigmadatingapp.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.sigmadatingapp.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var mViewPager: ViewPager2
+    private lateinit var pageIndicator: TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding_activity)
+
+        mViewPager = ViewPager2(this)
+        pageIndicator=TabLayout(this)
+        mViewPager.adapter = OnBoardingViewPagerAdapter(this, this)
+        mViewPager.offscreenPageLimit = 1
+        mViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                if (position == 2) {
+                   // btnNext.text = getText(R.string.finish)
+                } else {
+                   // btnNext.text = getText(R.string.next)
+                }
+            }
+
+            override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {}
+            override fun onPageScrollStateChanged(arg0: Int) {}
+        })
+        TabLayoutMediator(pageIndicator, mViewPager) { _, _ -> }.attach()
+
+       /* btnNext.setOnClickListener {
+            if (getItem() > mViewPager.childCount) {
+                finish()
+            } else {
+                mViewPager.setCurrentItem(getItem() + 1, true)
+            }
+        }
+
+        btnBack.setOnClickListener {
+            if (getItem() == 0) {
+                finish()
+            } else {
+                mViewPager.setCurrentItem(getItem() - 1, true)
+            }
+        }*/
+    }
+
+    private fun getItem(): Int {
+        return mViewPager.currentItem
+    }
+    }
+}
---
+++ app/src/main/java/com/example/sigmadatingapp/Activities/OnBoardingViewPagerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.sigmadatingapp.Activities
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.sigmadatingapp.R
+
+class OnBoardingViewPagerAdapter(
+    fragmentActivity: FragmentActivity,
+    private val context: Context
+) :
+    FragmentStateAdapter(fragmentActivity) {
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> BlankFragment.newInstance(
+                context.resources.getString(R.string.app_name),
+                context.resources.getString(R.string.app_name)
+            )
+            1 -> BlankFragment.newInstance(
+                context.resources.getString(R.string.app_name),
+                context.resources.getString(R.string.app_name)
+            )
+            else -> BlankFragment.newInstance(
+                context.resources.getString(R.string.app_name),
+                context.resources.getString(R.string.app_name)
+            )
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+}
